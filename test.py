8100e6c797201da2b4479bdd02a3b452dd82a0c6
Preston <childrep@vt.edu>
2019-05-26 14:51:08 -0400

    Updated gitignore to actually ignore secrets

--- test.py
+++ test.py
@@ -8,5 +8,5 @@ print(spreadsheet.event_info.contains_event('foo2'))
 spreadsheet.event_info.set_event_info(models.EventInfo('foo', '2019nyro', ['2791', '340', '3015', '5254', '20']))
 print(spreadsheet.event_info.get_event_info('foo'))
 spreadsheet.draft_results.set_pick(models.DraftResults(
-    'justin', 'foo', 1, 1, [models.Pick('8:30 PM', False, '254'), models.Pick('8:33 PM', True, '3044')]
+    'justin', 'foo', 1, 1, [models.Pick('8:30 PM', False, '254'), models.Pick('8:37 PM', True, '3044')]
 ))
